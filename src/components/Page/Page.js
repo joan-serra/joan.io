e7914088d978544a454e55775704939d0418b3af
joan-serra <joan.serra.molas@gmail.com>
2020-11-15 20:47:01 +0000

    fdsafsdaf fgnsadkf

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -6,8 +6,14 @@ function App() {
     <div class="container">
       <div class="subcontainer">
         <div class="stuffdiv">
-          <p class="stufftext">Hello, stranger.</p>
-          <p class="stufftextsmall">I am Joan, a human being who works in cloud stuff and enjoys things.</p> 
+          <p class="stufftext">Hello, stranger!</p>
+          <p class="stufftextsmall">I am Joan, and this is my sort of website. I am a human being who works in cloud stuff and enjoys many things. If you're curious, why not check any of the following...</p> 
+        </div>
+        <div class="stuffdiv">
+          <a class="stufflink" href="https://twitter.com/noopdles">Twitter</a>
+          <a class="stufflink" href="https://instagram.com/noopdles">Instagram</a>
+          <a class="stufflink" href="https://linkedin.com/">LinkedIn</a>
+          <a class="stufflink" href="https://github.com/joan-serra">GitHub</a>
         </div>
       </div>
     </div>
