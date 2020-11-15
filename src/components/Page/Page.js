7a57c86e6d62f53279586770d60310af952286bd
joan-serra <joan.serra.molas@gmail.com>
2020-11-15 02:53:10 +0000

    wip fdfdgfsg

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -4,12 +4,13 @@ import './Page.css';
 function App() {
   return (
     <div class="container">
+      <div class="subcontainer">
       <div class="stuffdiv">
-        <p  class="stufftext">Hello, stranger.</p>
-        <p class="stufftextsmall">I am a human being who works in cloud stuff and enjoys things.</p> 
+        <p class="stufftextsmall">Hello, stranger. <br /><br />I am Joan, a human being who works in cloud stuff and enjoys things.</p> 
       </div>
       <div class="stuffdiv">
         
+      </div>
       </div>
     </div>
   );
