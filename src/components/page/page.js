59d320f553ca8066d3a440179828cc3c76e7cf7a
joan-serra <joan.serra.molas@gmail.com>
2020-11-15 22:45:54 +0000

    cool icons n stuff

---
+++ src/components/page/page.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Page.css';
+import { FaLinkedIn, FaTwitter, FaTwitch, FaGitHub, FaInstagram } from "react-icons/fa";
+
+function App() {
+  return (
+    <div class="container">
+      <div class="subcontainer">
+        <div class="stuffdiv">
+          <p class="stufftext">Hello, stranger!</p>
+          <p class="stufftextsmall">I am Joan Serra, and this is my sort of website. I am a human being who works in cloud stuff and enjoys many things. If you're curious, why not check any of the following...</p> 
+        </div>
+        <div class="stuffdiv">
+          <a class="stufflink" href="https://twitter.com/noopdles" title="Drama and general unhingement. Please don't judge me."><FaTwitter />Twitter</a>
+          <a class="stufflink" href="https://twitch.tv/cybernoodles" title="I play videogames. Badly"><FaTwitch />Twitch</a>
+          <a class="stufflink" href="https://instagram.com/noopdles" title="Sometimes I may take a photo of something interesting."><FaInstagram />Instagram</a>
+          <a class="stufflink" href="https://linkedin.com/in/joanserramolas" title="seriouscat.jpg"><FaLinkedIn />LinkedIn</a>
+          <a class="stufflink" href="https://github.com/joan-serra" title="Code, or attempts at such."><FaGitHub />GitHub</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
