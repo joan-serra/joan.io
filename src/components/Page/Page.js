6805f98121af18894b53e4ae0f36e0a7bf33fda6
joan-serra <joan.serra.molas@gmail.com>
2020-11-15 22:57:34 +0000

    make things look a bit better

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -11,11 +11,11 @@ function App() {
           <p class="stufftextsmall">I am Joan Serra, and this is my sort of website. I am a human being who works in cloud stuff and enjoys many things. If you're curious, why not check any of the following...</p> 
         </div>
         <div class="stuffdiv">
-          <a class="stufflink" href="https://twitter.com/noopdles" title="Drama and general unhingement. Please don't judge me."><FaTwitter />Twitter</a>
-          <a class="stufflink" href="https://twitch.tv/cybernoodles" title="I play videogames. Badly"><FaTwitch />Twitch</a>
-          <a class="stufflink" href="https://instagram.com/noopdles" title="Sometimes I may take a photo of something interesting."><FaInstagram />Instagram</a>
-          <a class="stufflink" href="https://linkedin.com/in/joanserramolas" title="seriouscat.jpg"><FaLinkedin />LinkedIn</a>
-          <a class="stufflink" href="https://github.com/joan-serra" title="Code, or attempts at such."><FaGithub />GitHub</a>
+          <a class="stufflink" href="https://twitter.com/noopdles" title="Drama and general unhingement. Please don't judge me."><FaTwitter /> Twitter</a>
+          <a class="stufflink" href="https://twitch.tv/cybernoodles" title="I play videogames. Badly"><FaTwitch /> Twitch</a>
+          <a class="stufflink" href="https://instagram.com/noopdles" title="Sometimes I may take a photo of something interesting."><FaInstagram /> Instagram</a>
+          <a class="stufflink" href="https://linkedin.com/in/joanserramolas" title="seriouscat.jpg"><FaLinkedin /> LinkedIn</a>
+          <a class="stufflink" href="https://github.com/joan-serra" title="Code, or attempts at such."><FaGithub /> GitHub</a>
         </div>
       </div>
     </div>
