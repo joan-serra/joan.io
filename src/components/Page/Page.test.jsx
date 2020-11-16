b2710fe08c77d705ffe511abe9c7b4341d495a6f
joan-serra <joan.serra.molas@gmail.com>
2020-11-16 21:35:48 +0000

    remove test

--- src/components/Page/Page.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Page from './Page';
-
-test('just checking', () => {
-  const { getByText } = render(<Page />);
-  const linkElement = getByText(/Hello, stranger/i);
-  expect(linkElement).toBeInTheDocument();
-});
