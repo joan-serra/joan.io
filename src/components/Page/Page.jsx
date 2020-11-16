8f623b1bbaa4bf94f8766a03ba564ac1b1d23394
joan-serra <joan.serra.molas@gmail.com>
2020-11-16 22:32:52 +0000

    add bideo james

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Page.css';
 import Sparkles from './Sparkles';
-import { FaLinkedin, FaTwitter, FaTwitch, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaTwitter, FaTwitch, FaGithub, FaInstagram, FaSteam, FaBattleNet } from "react-icons/fa";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <div class="subcontainer">
         <div class="stuffdiv">
         <Sparkles><p class="stufftext">Hello, stranger!</p></Sparkles>
-          <p class="stufftextsmall">I am Joan Serra, and this is my sort of website. I am a human being who works in cloud stuff and enjoys many things. If you're curious, why not check any of the following...</p> 
+          <p class="stufftextsmall">I am Joan Serra, and this is my sort of website. I am a human being who works in cloud stuff and enjoys many things. If you're curious, why not check any of the following?</p> 
         </div>
         <div class="stuffdiv">
           <a class="stufflink" href="https://twitter.com/noopdles" title="Drama and general unhingement. Please don't judge me."><FaTwitter /> Twitter</a>
@@ -17,6 +17,11 @@ function App() {
           <a class="stufflink" href="https://instagram.com/noopdles" title="Sometimes I may take a photo of something interesting."><FaInstagram /> Instagram</a>
           <a class="stufflink" href="https://linkedin.com/in/joanserramolas" title="seriouscat.jpg"><FaLinkedin /> LinkedIn</a>
           <a class="stufflink" href="https://github.com/joan-serra" title="Code, or attempts at such."><FaGithub /> GitHub</a>
+          <br />
+          <p class="stufftextsmall">Fancy playing videogames with me?</p>
+          <br />
+          <a class="stufflink" href="https://steamcommunity.com/id/noopdles/" title="Don't judge me."><FaSteam /> Steam</a>
+          <a class="stufflink" href="https://playoverwatch.com/en-us/career/pc/Noodles-22366/" title="Mostly Overwatch and WoW"><FaBattleNet /> Noodles#22366 (Europe)</a>
         </div>
       </div>
     </div>
